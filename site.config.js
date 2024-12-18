773072b013549db807c59266fbc8e3cfbc545345
cho4u4o <cho4u4o@gmail.com>
2024-12-18 19:25:07 +0900

    docs : fix configuration

--- site.config.js
+++ site.config.js
@@ -28,11 +28,11 @@ const CONFIG = {
   blog: {
     title: "cho4u4o.log",
     description: "welcome to cho4u4ooo",
-    scheme: "light",
+    scheme: "dark",
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "https://cho4u4o.vercel.app/",
   since: 2023, // If leave this empty, current year will be used.
   lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
