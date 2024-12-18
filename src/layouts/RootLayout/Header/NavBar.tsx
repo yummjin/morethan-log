e26e32eea1538f8765ce78b0ef98fe4d68c51084
cho4u4o <cho4u4o@gmail.com>
2024-12-18 19:59:11 +0900

    feat : delete navbar contents

--- src/layouts/RootLayout/Header/NavBar.tsx
+++ src/layouts/RootLayout/Header/NavBar.tsx
@@ -2,7 +2,9 @@ import styled from "@emotion/styled"
 import Link from "next/link"
 
 const NavBar: React.FC = () => {
-  const links = [{ id: 1, name: "About", to: "/about" }]
+  const links = [
+    // { id: 1, name: "About", to: "/about" }
+  ]
   return (
     <StyledWrapper className="">
       <ul>
